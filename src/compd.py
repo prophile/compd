6f6ae431c050c4981a1556ce1b723e869929c96d
Alastair Lynn <arplynn@gmail.com>
2013-03-06 04:42:14 +0000

    Add command-line argument parsing

--- src/compd.py
+++ src/compd.py
@@ -1,4 +1,20 @@
-import txredisapi
+"""compd
 
-print "Hello, world!"
+Usage:
+  compd.py [options]
+
+Options:
+  --version                  Print the version, and exit.
+  -h --help                 Display this help.
+  -c <file> --config <file> Use an alternative config.yaml.
+"""
+
+from docopt import docopt
+from subprocess import check_output
+import sys
+
+GIT_VERSION = check_output(('git', 'describe', '--always')).strip()
+VERSION = 'compd {0}'.format(GIT_VERSION)
+
+options = docopt(__doc__, version = VERSION)
 
